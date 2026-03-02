448840cdca48a122e186bb95f8b65fc517024f95
heini208 <mstemmeler@gmx.de>
2026-03-03 00:59:24 +0100

    Add Circuits code generation

--- RobotMbed/src/main/java/de/fhg/iais/roberta/visitor/validate/CalliopeV3ValidatorAndCollectorVisitor.java
+++ RobotMbed/src/main/java/de/fhg/iais/roberta/visitor/validate/CalliopeV3ValidatorAndCollectorVisitor.java
@@ -71,151 +71,180 @@ public class CalliopeV3ValidatorAndCollectorVisitor extends CalliopeCommonValida
 
     @Override
     public Void visitSimulationJob(SimulationJob simulationJob) {
-        usedMethodBuilder.addUsedMethod(CalliopeMethods.SETUP_WIFI);
-        usedMethodBuilder.addUsedMethod(CalliopeMethods.PARSE_LIST_RESPONSE);
-        usedMethodBuilder.addUsedMethod(CalliopeMethods.SEND_AND_WAIT);
-        usedMethodBuilder.addUsedMethod(CalliopeMethods.SIMULATE_QBIT_MEASURE);
-        usedHardwareBuilder.addUsedActor(new UsedActor(SC.WIFI, SC.WIFI));
-        usedHardwareBuilder.addUsedActor(new UsedActor(SC.QISKIT, SC.QISKIT));
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_LIST);
         return null;
     }
 
     @Override
-    public Void visitIBMJob(IBMJob ibmjob) {
-        usedMethodBuilder.addUsedMethod(CalliopeMethods.SETUP_WIFI);
-        usedMethodBuilder.addUsedMethod(CalliopeMethods.SEND_AND_WAIT);
-        usedMethodBuilder.addUsedMethod(CalliopeMethods.GET_CLEAN_RESPONSE);
-        usedMethodBuilder.addUsedMethod(CalliopeMethods.START_REAL_QBIT_JOB);
-
-        usedHardwareBuilder.addUsedActor(new UsedActor(SC.WIFI, SC.WIFI));
+    public Void visitIBMJob(IBMJob ibmJob) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_STRING);
         usedMethodBuilder.addUsedMethod(CalliopeMethods.SETUP_IBM);
         usedHardwareBuilder.addUsedActor(new UsedActor(SC.IBM, SC.IBM));
-
-        usedHardwareBuilder.addUsedActor(new UsedActor(ibmjob.getUserDefinedPort(), SC.QISKIT));
         return null;
     }
 
     @Override
     public Void visitIBMJobResult(IBMJobResult ibmJobResult) {
-        usedMethodBuilder.addUsedMethod(CalliopeMethods.SETUP_WIFI);
-        usedMethodBuilder.addUsedMethod(CalliopeMethods.PARSE_LIST_RESPONSE);
+        setup_Wifi();
         usedMethodBuilder.addUsedMethod(CalliopeMethods.SETUP_IBM);
-        usedMethodBuilder.addUsedMethod(CalliopeMethods.IBM_GET_JOB_RESULT);
-
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_LIST);
         usedHardwareBuilder.addUsedActor(new UsedActor(SC.IBM, SC.IBM));
-
-        usedMethodBuilder.addUsedMethod(CalliopeMethods.SEND_AND_WAIT);
-        usedHardwareBuilder.addUsedActor(new UsedActor(SC.WIFI, SC.WIFI));
         usedHardwareBuilder.addUsedActor(new UsedActor(ibmJobResult.getUserDefinedPort(), SC.QISKIT));
         return null;
     }
 
     @Override
     public Void visitIBMJobStatus(IBMJobStatus ibmJobStatus) {
-        usedMethodBuilder.addUsedMethod(CalliopeMethods.SETUP_WIFI);
-        usedMethodBuilder.addUsedMethod(CalliopeMethods.SEND_AND_WAIT);
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_STRING);
         usedMethodBuilder.addUsedMethod(CalliopeMethods.SETUP_IBM);
-        usedMethodBuilder.addUsedMethod(CalliopeMethods.GET_CLEAN_RESPONSE);
-        usedMethodBuilder.addUsedMethod(CalliopeMethods.IBM_GET_JOB_STATUS);
         usedHardwareBuilder.addUsedActor(new UsedActor(SC.IBM, SC.IBM));
-
-        usedHardwareBuilder.addUsedActor(new UsedActor(SC.WIFI, SC.WIFI));
         usedHardwareBuilder.addUsedActor(new UsedActor(ibmJobStatus.getUserDefinedPort(), SC.QISKIT));
         return null;
     }
 
     @Override
     public Void visitRunCircuitSim(RunCircuitSim runCircuitSim) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 
     @Override
     public Void visitRunCircuitIBM(RunCircuitIBM runCircuitIBM) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.SETUP_IBM);
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
+        usedHardwareBuilder.addUsedActor(new UsedActor(SC.IBM, SC.IBM));
         return null;
     }
 
     @Override
     public Void visitGetJobResultSim(GetJobResultSim getJobResultSim) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_LIST);
         return null;
+    }
+
+    private void setup_Wifi(){
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.SETUP_WIFI);
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.SEND_AND_WAIT);
+        usedHardwareBuilder.addUsedActor(new UsedActor(SC.WIFI, SC.WIFI));
+        usedHardwareBuilder.addUsedActor(new UsedActor(SC.QISKIT, SC.QISKIT));
     }
 
     @Override
     public Void visitCreateCircuit(CreateCircuit createCircuit) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 
     @Override
     public Void visitCloneCircuit(CloneCircuit cloneCircuit) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 
     @Override
     public Void visitMeasureQubit(MeasureQubit measure) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 
     @Override
     public Void visitMeasureAllQubits(MeasureAllQubits measureAll) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 
     @Override
     public Void visitDeleteCircuit(DeleteCircuit deleteCircuit) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 
     @Override
     public Void visitResetCircuit(ResetCircuit resetCircuit) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 
+    // Gates
     @Override
     public Void visitXGate(XGate xGate) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 
     @Override
     public Void visitHGate(HGate hGate) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 
     @Override
     public Void visitZGate(ZGate zGate) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 
     @Override
     public Void visitYGate(YGate yGate) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 
     @Override
     public Void visitRXGate(RXGate rxGate) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 
     @Override
     public Void visitRYGate(RYGate ryGate) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 
     @Override
     public Void visitRZGate(RZGate rzGate) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 
     @Override
     public Void visitCXGate(CXGate cxGate) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 
     @Override
     public Void visitCZGate(CZGate czGate) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 
     @Override
     public Void visitSwapGate(SwapGate swapGate) {
+        setup_Wifi();
+        usedMethodBuilder.addUsedMethod(CalliopeMethods.CMD_OK);
         return null;
     }
 }
